c0ab4c139e731dbdb889271abfe4ea28455d3cee
Ben Ramchandani <ben.ramchandani@gmail.com>
2016-08-14 19:03:12 +0200

    Added basic tracing routine.

--- src/trace.rs
+++ src/trace.rs
@@ -2,6 +2,13 @@ use vec3::Vector3;
 use super::Ray;
 use shape::Shape;
 
+const RES_W: i32 = 40;
+const RES_H: i32 = 20;
+const SCREEN_Z: f64 = -2.0;
+const SCREEN_W: f64 = 2.0;
+const SCREEN_H: f64 = 2.0;
+const VIEW_Z: f64 = -5.0;
+
 
 pub fn trace_nearest<'a>(ray: Ray, shapes: &'a Vec<&Shape>)
         -> Option<(&'a Shape, Vector3)> {
@@ -26,23 +33,21 @@ pub fn trace_nearest<'a>(ray: Ray, shapes: &'a Vec<&Shape>)
     });
 }
 
-fn gen_screen(i: i32, j: i32) -> Vector3 {
-    let top_left = Vector3::new(-1.0, 1.0, -2.0);
-    let width = Vector3::new(2.0, 0.0, 0.0);
-    let height = Vector3::new(0.0, -2.0, 0.0);
-    let (resolution_w, resolution_h): (f64, f64) = (5.0, 5.0);
-    
-    let increment_w = width / resolution_w;
-    let increment_h = height / resolution_h;
-    return top_left + increment_w * (j as f64) + increment_h * (i as f64);
-}
-
-pub fn print_screen() {
-    for i in 0..6 {
-        for j in 0..6 {
-            println!("{:?}", gen_screen(i, j));
-        }
-        println!("");
+// This is a terrible name
+fn get_view_ray_fn() -> Box<Fn(i32, i32) -> Ray> {
+    let width = Vector3::new(SCREEN_W, 0.0, 0.0);
+    let height = Vector3::new(0.0, -SCREEN_H, 0.0);
+    let resolution_w = RES_W as f64;
+    let resolution_h = RES_H as f64;
+    let eye = Vector3::new(0.0, 0.0, VIEW_Z);
+    {
+        let top_left = Vector3::new(-SCREEN_W/2.0, SCREEN_H/2.0, SCREEN_Z);
+        let increment_w = width / resolution_w;
+        let increment_h = height / resolution_h;
+        return Box::new(move |i: i32, j:i32| {
+            let screen_point = top_left + increment_w * (j as f64) + increment_h * (i as f64);
+            return ((screen_point - eye).normalize(), eye);
+        });
     }
 }
 
@@ -55,11 +60,11 @@ pub fn simple_trace() {
     let p = Plane {normal: Vector3::new(0.0, 1.0, 0.0), origin_distance: -5.0};
     let world: Vec<&Shape> = vec![&p, &s];
     let mut pixels: Vec<Vec<Color>> = vec![];
-    let eye = Vector3::new(0.0, 0.0, -5.0);
-    for i in 0..6 {
+    let gen_screen: Box<Fn(i32, i32) -> Ray> = get_view_ray_fn();
+    for i in 0..(RES_H+1) {
         let mut row: Vec<Color> = vec![];
-        for j in 0..6 {
-            let ray = ((gen_screen(i, j) - eye).normalize(), eye);
+        for j in 0..(RES_W + 1) {
+            let ray = gen_screen(i, j);
             if let Some(_) = trace_nearest(ray, &world) {
                 row.push(Color::from_rgb(255, 0, 0));
             } else {
